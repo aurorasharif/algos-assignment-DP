2d29b0ee29e9ccbf5f269db1b72c2adb9c183d14
Aurora Sharif <auro@Auroras-MacBook-Pro.local>
2019-04-02 15:26:01 -0400

    rod cutting added

--- RodCutting.java
+++ RodCutting.java
@@ -1,18 +1,29 @@
-/**
- * Rod cutting problem described in Chapter 15 of textbook
- */
+
 public class RodCutting {
 
-  // Do not change the parameters!
-  public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
-  }
+      // Do not change the parameters!
+      public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
+        if (rodLength <= 0) 
+                return 0; 
+            int max_val = Integer.MIN_VALUE; 
+            for (int i = 0; i<rodLength; i++) 
+                max_val = Math.max(max_val, 
+                    lengthPrices[i] + rodCuttingRecur(rodLength-i-1, lengthPrices)); 
+        return max_val;
+      }
 
-  // Do not change the parameters!
-  public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
-  }
-
+      // Do not change the parameters!
+      public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
+           int val[] = new int[rodLength+1]; 
+          val[0] = 0; 
+          for (int i = 1; i<=rodLength; i++) { 
+            int max_val = Integer.MIN_VALUE; 
+            for (int j = 0; j < i; j++) 
+              max_val = Math.max(max_val, 
+                  lengthPrices[j] + val[i-j-1]); 
+            val[i] = max_val; 
+          } return val[rodLength];
+        }
 
   public static void main(String args[]){
       RodCutting rc = new RodCutting();
